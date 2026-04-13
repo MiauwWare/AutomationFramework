063af5ab5a253d64301d1d1eeb0f92507c1941dd
ru-the-dev <dev.ru.vero@gmail.com>
2026-04-13 18:13:28 +0200

    Add new template file names for TSM windows and character selection

--- AutomationRunner/VisionTemplateFileNames.cs
+++ AutomationRunner/VisionTemplateFileNames.cs
@@ -13,9 +13,12 @@ public static class VisionTemplateFileNames
     public const string TSM_MAIL_SELECTED_GROUPS_BTN = "tsm_mail_selected_groups_btn.png";
     public const string TSM_MAILBOX_GROUPS_BTN = "tsm_mailbox_groups_btn.png";
     public const string TSM_MAX_BTN = "tsm_max_btn.png";
+    public const string TSM_ENGINEERING_WINDOW = "tsm_engineering_window.png";
+    public const string TSM_MAIL_WINDOW = "tsm_mail_window.png";
     public const string AB_TSM_DESTROY_BTN = "ab_tsm_destroy_btn.png";
     public const string TSM_OPEN_ALL_MAIL = "tsm_open_all_mail.png";
     public const string MAIN_MENU_PASSWORD_INPUT = "main_menu_password_input.png";
     public const string MAIN_MENU_PASSWORD_TEXT = "main_menu_password_text.png";
     public const string MAIN_MENU_BUTTONS = "main_menu_buttons.png";
+    public const string CHARACTER_SELECT_ENTER_WORLD_BTN_ACTIVE = "character_select_enter_world_btn_active.png";
 }
