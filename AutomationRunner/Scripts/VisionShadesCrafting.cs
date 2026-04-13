b351f30b4b4317dc6aa1ab0fe7f0bc2b0bcf3ba6
ru-the-dev <dev.ru.vero@gmail.com>
2026-04-13 18:36:45 +0200

    Enhance VisionShadesCrafting script with new window handling and image matching logic for engineering and mail interactions

--- AutomationRunner/Scripts/VisionShadesCrafting.cs
+++ AutomationRunner/Scripts/VisionShadesCrafting.cs
@@ -1,5 +1,6 @@
 
 
+using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 using AutomationFramework;
 using AutomationFramework.Extensions;
@@ -44,6 +45,8 @@ public sealed class VisionShadesCrafting : BaseScript
         AcquireTemplates
         (
             VisionTemplateFileNames.AB_ENGINEERING_BTN,
+            VisionTemplateFileNames.TSM_ENGINEERING_WINDOW,
+            VisionTemplateFileNames.TSM_MAIL_WINDOW,
             VisionTemplateFileNames.TSM_MAX_BTN,
             VisionTemplateFileNames.TSM_CRAFT_BTN,
             VisionTemplateFileNames.TSM_CLOSE_BTN,
@@ -80,24 +83,24 @@ public sealed class VisionShadesCrafting : BaseScript
 
         while (cancellationToken.IsCancellationRequested == false)
         {
-            // Open crafting window
-            var engineeringButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_ENGINEERING_BTN].TemplateMat, 0.7, cancellationToken: cancellationToken, searchRegion: searchRegion);
-            if (engineeringButtonImageMatch is null)
+            var engineeringWindowResult = await OpenEngineeringWindowAsync(searchRegion!.Value, cancellationToken: cancellationToken);
+
+            if (engineeringWindowResult.Success is false)
             {
-                _logger.LogWarning("Engineering button not found.");
-                return;
+                break;
             }
 
-            var engineeringButtonRandomPoint = engineeringButtonImageMatch.ToGlobalBounds().Inset(20).GetRandomPointInBounds();
-            await _cursor.MoveToAsync(engineeringButtonRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
-
-
-            // Wait for window to open
-            await Task.Delay(TimeSpan.FromSeconds(Random.Shared.NextFloat(2, 3)), cancellationToken);
+            var engineeringWindowBounds = engineeringWindowResult.Match.ToGlobalBounds();
 
             // Click TSM max button
-            var maxButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_MAX_BTN].TemplateMat, 0.7, cancellationToken: cancellationToken, searchRegion: searchRegion);
+            var maxButtonImageMatch = await _vision.FindImageAsync
+            (
+                _templates[VisionTemplateFileNames.TSM_MAX_BTN].TemplateMat,
+                0.7, 
+                cancellationToken: cancellationToken, 
+                searchRegion: engineeringWindowBounds
+            );
+
             if (maxButtonImageMatch == null)
             {
                 _logger.LogWarning("Max button not found.");
@@ -105,17 +108,17 @@ public sealed class VisionShadesCrafting : BaseScript
             }
 
             var maxButtonBounds = maxButtonImageMatch.ToGlobalBounds();
-            maxButtonBounds = maxButtonBounds.Translate(-3 * maxButtonBounds.Width, 0);
+            maxButtonBounds = maxButtonBounds.Scale(1, 0.75f).Translate(-3 * maxButtonBounds.Width, 0);
 
             var quantityInputRandomPoint = maxButtonBounds.GetRandomPointInBounds();
             await _cursor.MoveToAsync(quantityInputRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
 
             // input the quantity to craft, which is 200 in this case
             await _keyboard.TypeTextAsync("200", cancellationToken: cancellationToken);
 
             // Click TSM craft button
-            var craftButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_CRAFT_BTN].TemplateMat, 0.8, cancellationToken: cancellationToken, searchRegion: searchRegion);
+            var craftButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_CRAFT_BTN].TemplateMat, 0.8, cancellationToken: cancellationToken, searchRegion: engineeringWindowBounds);
             if (craftButtonImageMatch == null)
             {
                 _logger.LogWarning("Craft button not found.");
@@ -124,11 +127,11 @@ public sealed class VisionShadesCrafting : BaseScript
 
             var tsmCraftButtonRandomPoint = craftButtonImageMatch.ToGlobalBounds().Padd(30, 5).GetRandomPointInBounds();
             await _cursor.MoveToAsync(tsmCraftButtonRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
 
 
             // wait for crafts to be complete about 100 sec
-            await Task.Delay(TimeSpan.FromSeconds(175).ApplyRandomFactor(0.9, 1), cancellationToken);
+            await Task.Delay(TimeSpan.FromSeconds(150).ApplyRandomFactor(0.9, 1.1), cancellationToken);
 
             // Click TSM close button
             var tsmCloseButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_CLOSE_BTN].TemplateMat, 0.70, cancellationToken: cancellationToken, searchRegion: searchRegion);
@@ -139,51 +142,31 @@ public sealed class VisionShadesCrafting : BaseScript
             }
 
 
-            var tsmCloseButtonRandomPoint = tsmCloseButtonImageMatch.ToGlobalBounds().Inset(3).GetRandomPointInBounds();
-            await _cursor.MoveToAsync(tsmCloseButtonRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
+            var tsmCloseButtonCenter = tsmCloseButtonImageMatch.ToGlobalBounds().Center();
+            await _cursor.MoveToAsync(tsmCloseButtonCenter, cancellationToken: cancellationToken);
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor(), cancellationToken: cancellationToken);
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
 
             // wait just a second for a possible last craft to complete
-            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            await Task.Delay(TimeSpan.FromSeconds(2).ApplyRandomFactor(), cancellationToken);
 
-            // mount up on the gilded brutosaur
-            var gildedTradersBrutosaurImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_GILDED_TRADERS_BRUTOSAUR_BTN].TemplateMat, 0.60, cancellationToken: cancellationToken, searchRegion: searchRegion);
-            if (gildedTradersBrutosaurImageMatch == null)
+            var mountRes = await MountBrutosaurAsync(searchRegion!.Value);
+            if (mountRes is false)
             {
-                _logger.LogWarning("GildedTradersBrutosaur not found.");
+                _logger.LogInformation("Failed to mount the brutosaur.");
                 return;
             }
 
-            var gildedTradersBrutosaurRandomPoint = gildedTradersBrutosaurImageMatch.ToGlobalBounds().Inset(20).GetRandomPointInBounds();
-            await _cursor.MoveToAsync(gildedTradersBrutosaurRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
-
-            // wait for mount to be summoned
-            await Task.Delay(TimeSpan.FromSeconds(3).ApplyRandomFactor(0.8, 1.2), cancellationToken);
-
-            // target the mail NPC via macro
-            var targetMailButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_TARGET_MAIL_NPC_BTN].TemplateMat, 0.70, cancellationToken: cancellationToken, searchRegion: searchRegion);
-            if (targetMailButtonImageMatch == null)
+            var mailWindowRes = await OpenMailWindowAsync(searchRegion!.Value, cancellationToken: cancellationToken);
+            if (mailWindowRes.Success is false)
             {
-                _logger.LogWarning("TargetMailButton not found.");
-                return;
+                break;
             }
 
-            var targetMailButtonRandomPoint = targetMailButtonImageMatch.ToGlobalBounds().Inset(20).GetRandomPointInBounds();
-            await _cursor.MoveToAsync(targetMailButtonRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
-
-            // simulate human brain processing time xD
-            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor(0.8, 1.2), cancellationToken);
-
-            // interact with the mail NPC (with the interact keybind)
-            await _keyboard.PressKeyAsync(VirtualKey.F, cancellationToken: cancellationToken);
-
-            // wait for mail window to open
-            await Task.Delay(TimeSpan.FromSeconds(0.5), cancellationToken);
+            var mailWindowBounds = mailWindowRes.Match.ToGlobalBounds();
 
             // click the TSM mailbox groups button
-            var tsmMailboxGroupsImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_MAILBOX_GROUPS_BTN].TemplateMat, 0.7, cancellationToken: cancellationToken, searchRegion: searchRegion);
+            var tsmMailboxGroupsImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_MAILBOX_GROUPS_BTN].TemplateMat, 0.7, cancellationToken: cancellationToken, searchRegion: mailWindowBounds);
             if (tsmMailboxGroupsImageMatch == null)
             {
                 _logger.LogWarning("Groups button not found.");
@@ -192,25 +175,27 @@ public sealed class VisionShadesCrafting : BaseScript
 
             var tsmMailboxGroupsButtonRandomPoint = tsmMailboxGroupsImageMatch.ToGlobalBounds().Inset(10).GetRandomPointInBounds();
             await _cursor.MoveToAsync(tsmMailboxGroupsButtonRandomPoint, cancellationToken: cancellationToken);
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor(), cancellationToken);
             await _cursor.ClickAsync();
 
             //wait 1 sec, to ensure tab switch
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            await Task.Delay(TimeSpan.FromSeconds(2).ApplyRandomFactor());
 
             // click the TSM mail selected groups button
-            var tsmMailSelectedGroupsImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_MAIL_SELECTED_GROUPS_BTN].TemplateMat, 0.50, cancellationToken: cancellationToken, searchRegion: searchRegion);
+            var tsmMailSelectedGroupsImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_MAIL_SELECTED_GROUPS_BTN].TemplateMat, 0.50, cancellationToken: cancellationToken, searchRegion: mailWindowBounds);
             if (tsmMailSelectedGroupsImageMatch == null)
             {
                 _logger.LogWarning("Mail Selected Groups button not found.");
                 return;
             }
 
-            var tsmMailSelectedGroupsButtonRandomPoint = tsmMailSelectedGroupsImageMatch.ToGlobalBounds().Padd(100, 5).GetRandomPointInBounds();
+            var tsmMailSelectedGroupsButtonRandomPoint = tsmMailSelectedGroupsImageMatch.ToGlobalBounds().Scale(2, 1).GetRandomPointInBounds();
             await _cursor.MoveToAsync(tsmMailSelectedGroupsButtonRandomPoint, cancellationToken: cancellationToken);
-            await _cursor.ClickAsync();
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor(), cancellationToken);
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
 
             // wait for all mail to send
-            await Task.Delay(TimeSpan.FromSeconds(16).ApplyRandomFactor(0.8, 1.2), cancellationToken);
+            await Task.Delay(TimeSpan.FromSeconds(20).ApplyRandomFactor(0.8, 1.2), cancellationToken);
         }
     }   
 
@@ -222,5 +207,159 @@ public sealed class VisionShadesCrafting : BaseScript
         {
             _templates[filename] = _vision.AcquireTemplateLease(filename);
         }
+    }
+
+
+    public sealed class OpenWindowResult
+    {
+        public ImageMatchResult? Match { get; init; } // replace object with the real FindImageAsync match type
+
+        [MemberNotNullWhen(true, nameof(Match))]
+        public bool Success => Match is not null;
+    }
+    private async Task<OpenWindowResult> OpenEngineeringWindowAsync(Rectangle searchRegion, int maxAttempts = 3, CancellationToken cancellationToken = default)
+    {
+        ImageMatchResult? engineeringWindowMatch = null;
+
+        for (int attempt = 1; attempt <= maxAttempts; ++attempt)
+        {
+            // move mouse to center-ish with a random offset between -100 to 100
+            var newPos = searchRegion.Center() + new Vector2(Random.Shared.Next(-100, 100), Random.Shared.Next(-100, 100));
+            await _cursor.MoveToAsync(newPos);
+
+            // find the engineering AB button
+            var engineeringButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_ENGINEERING_BTN].TemplateMat, 0.7, cancellationToken: cancellationToken, searchRegion: searchRegion);
+            if (engineeringButtonImageMatch is null)
+            {
+                _logger.LogWarning("Engineering button not found.");
+
+                continue;
+            }
+
+            //move mouse over engineering button
+            var engineeringButtonRandomPoint = engineeringButtonImageMatch.ToGlobalBounds().Inset(20).GetRandomPointInBounds();
+            await _cursor.MoveToAsync(engineeringButtonRandomPoint, cancellationToken: cancellationToken);
+
+            // click it
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor());
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
+
+            // Wait for window to open
+            await Task.Delay(TimeSpan.FromSeconds(Random.Shared.NextFloat(5, 8)), cancellationToken);
+
+
+            // Check if the window is open and detected
+            engineeringWindowMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_ENGINEERING_WINDOW].TemplateMat, 0.5, searchRegion, cancellationToken: cancellationToken);
+
+            if (engineeringWindowMatch is not null)
+            {
+                break;
+            }
+        }
+
+        
+        return new OpenWindowResult()
+        {
+            Match = engineeringWindowMatch
+        };
+    }
+
+    private async Task<bool> MountBrutosaurAsync(Rectangle searchRegion, int maxAttempts = 3, CancellationToken cancellationToken = default)
+    {
+        // buffs are always in the top half of the screen
+        var buffSearchRegion = new Rectangle(searchRegion.X, searchRegion.Y, searchRegion.Width, (int)(searchRegion.Height / 4f));
+
+        // action bars are always in the bottom half of the screen
+        var abSearchRegion = new Rectangle(searchRegion.X, (int)(searchRegion.Y + searchRegion.Height * 0.75f), searchRegion.Width, (int)(searchRegion.Height / 4f));
+
+        bool success = false;
+
+        for (int attempt = 1; attempt <= maxAttempts; ++attempt)
+        {
+            // move mouse to center-ish with a random offset between -100 to 100
+            var newPos = searchRegion.Center() + new Vector2(Random.Shared.Next(-100, 100), Random.Shared.Next(-100, 100));
+            await _cursor.MoveToAsync(newPos);
+
+            //find the gilded brutosaur on the action bar
+            var mountAbImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_GILDED_TRADERS_BRUTOSAUR_BTN].TemplateMat, 0.60, cancellationToken: cancellationToken, searchRegion: abSearchRegion);
+            if (mountAbImageMatch is null)
+            {
+                _logger.LogWarning("GildedTradersBrutosaur not found.");
+                continue;
+            }
+
+            var gildedTradersBrutosaurRandomPoint = mountAbImageMatch.ToGlobalBounds().Inset(20).GetRandomPointInBounds();
+            await _cursor.MoveToAsync(gildedTradersBrutosaurRandomPoint, cancellationToken: cancellationToken);
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor(), cancellationToken: cancellationToken);
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
+
+            // wait for mount to be summoned
+            await Task.Delay(TimeSpan.FromSeconds(3).ApplyRandomFactor(0.8, 1.2), cancellationToken);
+
+            //assert the mount is found in the buff list
+            var mountBuffImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_GILDED_TRADERS_BRUTOSAUR_BTN].TemplateMat, 0.60, cancellationToken: cancellationToken, searchRegion: buffSearchRegion);
+            
+            if (mountBuffImageMatch is not null)
+            {
+                // the buff is found, therefore we are mounted on the brutosaur
+                success = true;
+                break;
+            }
+        }
+
+
+        return success;
+    }
+
+
+    private async Task<OpenWindowResult> OpenMailWindowAsync(Rectangle searchRegion, int maxAttempts = 3, CancellationToken cancellationToken = default)
+    {
+        ImageMatchResult? mailWindowMatch = null;
+
+        for (int attempt = 1; attempt <= maxAttempts; ++attempt)
+        {
+            // move mouse to center-ish with a random offset between -100 to 100
+            var newPos = searchRegion.Center() + new Vector2(Random.Shared.Next(-100, 100), Random.Shared.Next(-100, 100));
+            await _cursor.MoveToAsync(newPos);
+
+            // find the interact with mail npc AB button
+            var targetMailButtonImageMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.AB_TARGET_MAIL_NPC_BTN].TemplateMat, 0.7, cancellationToken: cancellationToken, searchRegion: searchRegion);
+            if (targetMailButtonImageMatch is null)
+            {
+                _logger.LogWarning("TargetMailButton not found.");
+
+                continue;
+            }
+
+            //move mouse over engineering button
+            var targetMailButtonRandomPoint = targetMailButtonImageMatch.ToGlobalBounds().Inset(20).GetRandomPointInBounds();
+            await _cursor.MoveToAsync(targetMailButtonRandomPoint, cancellationToken: cancellationToken);
+
+            // click it
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor());
+            await _cursor.ClickAsync(cancellationToken: cancellationToken);
+            
+            
+            // interact with the mail NPC (with the interact keybind)
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor());
+            await _keyboard.PressKeyAsync(VirtualKey.F, cancellationToken: cancellationToken);
+
+            // Wait for window to open
+            await Task.Delay(TimeSpan.FromSeconds(1).ApplyRandomFactor(), cancellationToken);
+
+
+            // Check if the window is open and detected
+            mailWindowMatch = await _vision.FindImageAsync(_templates[VisionTemplateFileNames.TSM_MAIL_WINDOW].TemplateMat, 0.5, searchRegion, cancellationToken: cancellationToken);
+
+            if (mailWindowMatch is not null)
+            {
+                break;
+            }
+        }
+        
+        return new OpenWindowResult()
+        {
+            Match = mailWindowMatch
+        };
     }
 }
